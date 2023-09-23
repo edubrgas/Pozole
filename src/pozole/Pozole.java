b94522aac0cdb2279813eb148339155b091651a5
Eduardo Muñoz <edu.mun.gon@gmail.com>
2023-09-22 18:59:37 -0600

    Archivos
    Solamente a;adi los archivos que estaban del profe

---
+++ src/pozole/Pozole.java
@@ -0,0 +1,18 @@
+package pozole;
+
+/**
+ *
+ * @author 
+ */
+public class Pozole 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Tablero t =new Tablero();
+        t.setVisible(true);
+    }        
+}
